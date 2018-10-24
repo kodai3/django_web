7996d7aec59dfcd46e8dc4c4160ed2fd9817938d
kodai3 <k3dai.su3@gmail.com>
2018-10-24 14:13:39 +0900

    static

--- my_django/first_project/first_app/views.py
+++ my_django/first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    my_dict = {'insert_me':' now im from first_app/index.html'}
+    return render(request, 'first_app/index.html', context=my_dict)
